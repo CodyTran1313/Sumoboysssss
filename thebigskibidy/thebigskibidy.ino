39df974a377d717a78551c415fa4b1c688550e42
zegoldengecko <alexxiang341@gmail.com>
2025-07-23 10:52:08 +1000

    Finished Code - testing

--- thebigskibidy/thebigskibidy.ino
+++ thebigskibidy/thebigskibidy.ino
@@ -18,26 +18,32 @@
 
 // TODO: Define constants/variables for motors (workshop 4)
 int RIGHT_SPEED = 10; // Speed pin, ranges from 0 to 255 (ENA)
-int RIGHT_F = 12; // Pin to move motor forwards (IN1)
-int RIGHT_R = 11; // Pin to move motor backwards (IN2)
+int RIGHT_F = 11; // Pin to move motor forwards (IN1)
+int RIGHT_R = 12; // Pin to move motor backwards (IN2)
 
 int LEFT_SPEED = 9; // Speed pin, ranges from 0 to 255 (ENB)
-int LEFT_F = 8; // Pin to move motor forwards (IN3)
-int LEFT_R = 7; // Pin to move motor backwards (IN4)
+int LEFT_F = 7; // Pin to move motor forwards (IN3)
+int LEFT_R = 8; // Pin to move motor backwards (IN4)
+
+int RIGHT_CHECK = 1; //Checking right first
 
 // TODO: Define other constants to be used in your sumobot
-#define MAX_SPEED 255
+#define MAX_SPEED 256
+#define PARTIAL_SPEED 90
 
 //Maximum distance robot can be, 999 for now until I'm told
-#define ROBOT_RANGE 999
+#define ROBOT_RANGE 120
 
 //Time it takes to drive around the circle
-#define Circle_time 1000
+#define Circle_time 1500
 
 #define WAITING 0
 #define SEARCHING 1
 #define ATTACKING 2
 #define SPEEDOFSENSOR 0.0340
+
+//Time to turn 90 degrees
+#define turn90 300
 
 // TODO: Initialise more global variables to be used
 int currentState = SEARCHING;
@@ -63,6 +69,7 @@ void setup() {
 // This function is where all your logic will go. The provided template uses the 
 // 'states model' discussed in week 5's build session.
 void loop() {
+
     //Stay in system state WAITING for 5 seconds
     if (currentState == WAITING) {
         Serial.println("Waiting 5 seconds before starting");
@@ -70,31 +77,15 @@ void loop() {
         currentState = SEARCHING;
     }
 
-    //OK SO IF WE'RE PULLING THAT CIRCLE DRIVING BS
-    double distanceDetected = getDistance(trigPin1, echoPin1);
-    if (distanceDetected <= ROBOT_RANGE) {
-        //seen as we're driving in a circle regardless, we can just note down the position here
-        Serial.println("enemy robot found");
-    }
-
-    //Turning left so we're ready to drive
-    while (checkBorder(IRPin) != 1) {
-        stationaryTurnLeft(MAX_SPEED);
-    }
-
-    //Now I have no fucking clue how to drive around the edge of the circle but I'll just wing it here
+    //Drive in a loop for x amount of time, should end up at the edge of the circle
     double startTime = millis();
     while (millis() <= startTime + Circle_time) {
-        if (checkBorder(IRPin) == 1) {
-            stationaryTurnRight(MAX_SPEED);
-        } else {
-            driveForwards(MAX_SPEED);
-        }
+        turnRightSlight(MAX_SPEED, PARTIAL_SPEED);
     }
 
-    //At this point we should (theoretically) be at the other side of the circle, so here we turn around
+    //At this point we should be at the edge of the cirlce, turn right 90 degrees so we're facing the centre
     stationaryTurnRight(MAX_SPEED);
-    delay(100);
+    delay(turn90);
     stop();
 
     //Now we need to start scanning for the robot
@@ -103,11 +94,10 @@ void loop() {
         Serial.println("Front White");
         driveBackwards(MAX_SPEED);
         delay(100);
-        turnRight(MAX_SPEED);
+        stationaryTurnRight(MAX_SPEED);
         delay(50);
     } else if (checkBorder(sideIRPin)) {
         Serial.println("Side White");
-        // ?? WHICH SIDE YOU FUCK, WRITE COMMENTS
         stationaryTurnRight(MAX_SPEED);
         delay(50);
         driveForwards(MAX_SPEED);
@@ -124,20 +114,22 @@ void loop() {
             //If/else conditionals based on detection
             if (distanceDetected <= ROBOT_RANGE) {
                 currentState = ATTACKING;
-            } else if(checkBorder(IRPin) != 1) {
-                // ^^^ pretty sure this won't work because idfk where the IR pin is,
-                // but basically it's meant to be, until it reaches the border turn right
-                // alternatively we should probably do it based on degrees
-
+            } else if(RIGHT_CHECK == 1) {
                 //Turn right first because it's likely the robot will be on our right (as we loop around the left)
                 stationaryTurnRight(MAX_SPEED);
+                delay(turn90);
+                RIGHT_CHECK = 0;
             } else {
                 stationaryTurnLeft(MAX_SPEED);
+                delay(turn90 + turn90);
+                RIGHT_CHECK = 1;
             }
         case ATTACKING:
             // If it finds bot, ram at it
             if (getDistance(trigPin1, echoPin1) <= ROBOT_RANGE) {
                 driveForwards(MAX_SPEED);
+            } else if (checkBorder(IRPin) == 1 && getDistance(trigPin1, echoPin1) <= ROBOT_RANGE) {
+                victorySpins();
             } else {
                 currentState = SEARCHING;
             }
@@ -181,6 +173,7 @@ double getDistance(int trigPin, int echoPin) {
     digitalWrite(trigPin, LOW);
     duration = pulseIn(echoPin, HIGH);
     distance = (duration * SPEEDOFSENSOR) / 2; // distance's unit is cm and duration is in ms 
+    Serial.println("Distance detected: " + String(distance) + " cm");
     return distance;
 }
 
@@ -193,12 +186,13 @@ double getDistance(int trigPin, int echoPin) {
 /   summary: uses IR sensor to detect edge of arena
 */
 int checkBorder(int irSensorPin) {
-	int statusSensor = digitalRead(irSensorPin);
-	if (statusSensor == HIGH) {
-		return 1;
-	} else {
-		return 0;
-	}
+    int statusSensor = digitalRead(irSensorPin);
+    if (statusSensor == HIGH) {
+        Serial.println("Detected 1");
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 // What if you have more than 1 ultrasonic or infrared sensor? Do you need more
@@ -216,13 +210,13 @@ int checkBorder(int irSensorPin) {
 void driveForwards(int speed)
 {
 Serial.println("Driving forward");
-	analogWrite(LEFT_SPEED, speed);
-	analogWrite(RIGHT_SPEED, speed);
+    analogWrite(LEFT_SPEED, speed);
+    analogWrite(RIGHT_SPEED, speed);
 
-	digitalWrite(LEFT_F, HIGH);
-	digitalWrite(LEFT_R, LOW);
-	digitalWrite(RIGHT_F, HIGH);
-	digitalWrite(RIGHT_R, LOW);
+    digitalWrite(LEFT_F, HIGH);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, HIGH);
+    digitalWrite(RIGHT_R, LOW);
 }
 // ADDITIONAL: How can we change the above function to all the sumobot to move
 // forward at a variable speed? HINT: Modify the analogWrite functions
@@ -235,13 +229,13 @@ Serial.println("Driving forward");
 void driveBackwards(int speed)
 {
     Serial.println("Driving backwards");
-	analogWrite(LEFT_SPEED, speed);
-	analogWrite(RIGHT_SPEED, speed);
+    analogWrite(LEFT_SPEED, speed);
+    analogWrite(RIGHT_SPEED, speed);
 
-	digitalWrite(LEFT_F, LOW);
-	digitalWrite(LEFT_R, HIGH);
-	digitalWrite(RIGHT_F, LOW);
-	digitalWrite(RIGHT_R, HIGH);
+    digitalWrite(LEFT_F, LOW);
+    digitalWrite(LEFT_R, HIGH);
+    digitalWrite(RIGHT_F, LOW);
+    digitalWrite(RIGHT_R, HIGH);
 }
 
 /*  Function: Turn left
@@ -250,29 +244,30 @@ void driveBackwards(int speed)
 /   summary: this function turns sumobot to the left
 */
 
+//Turn left in place
 void stationaryTurnLeft(int speed)
 {
     Serial.println("Turning left");
-	analogWrite(LEFT_SPEED, speed);
-	analogWrite(RIGHT_SPEED, speed);
+    analogWrite(LEFT_SPEED, speed);
+    analogWrite(RIGHT_SPEED, speed);
 
-	digitalWrite(LEFT_F, LOW);
-	digitalWrite(LEFT_R, HIGH);
-	digitalWrite(RIGHT_F, HIGH);
-	digitalWrite(RIGHT_R, LOW);
+    digitalWrite(LEFT_F, LOW);
+    digitalWrite(LEFT_R, HIGH);
+    digitalWrite(RIGHT_F, HIGH);
+    digitalWrite(RIGHT_R, LOW);
 }
 
-
+//Turn left with an arc
 void turnLeft(int speed)
 {
     Serial.println("Moving left");
-	analogWrite(LEFT_SPEED, speed);
-	analogWrite(RIGHT_SPEED, speed);
+    analogWrite(LEFT_SPEED, speed);
+    analogWrite(RIGHT_SPEED, speed);
 
-	digitalWrite(LEFT_F, LOW);
-	digitalWrite(LEFT_R, LOW);
-	digitalWrite(RIGHT_F, HIGH);
-	digitalWrite(RIGHT_R, LOW);
+    digitalWrite(LEFT_F, LOW);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, HIGH);
+    digitalWrite(RIGHT_R, LOW);
 }
 
 
@@ -282,25 +277,38 @@ void turnLeft(int speed)
 void stationaryTurnRight(int speed)
 {
     Serial.println("Turning right");
-	analogWrite(LEFT_SPEED, speed);
-	analogWrite(RIGHT_SPEED, speed);
+    analogWrite(LEFT_SPEED, speed);
+    analogWrite(RIGHT_SPEED, speed);
 
-	digitalWrite(LEFT_F, HIGH);
-	digitalWrite(LEFT_R, LOW);
-	digitalWrite(RIGHT_F, LOW);
-	digitalWrite(RIGHT_R, HIGH);
+    digitalWrite(LEFT_F, HIGH);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, LOW);
+    digitalWrite(RIGHT_R, HIGH);
 }
 
 void turnRight(int speed)
 {
     Serial.println("Moving right");
-	analogWrite(LEFT_SPEED, speed);
-	analogWrite(RIGHT_SPEED, speed);
+    analogWrite(LEFT_SPEED, speed);
+    analogWrite(RIGHT_SPEED, speed);
 
-	digitalWrite(LEFT_F, HIGH);
-	digitalWrite(LEFT_R, LOW);
-	digitalWrite(RIGHT_F, LOW);
-	digitalWrite(RIGHT_R, LOW);
+    digitalWrite(LEFT_F, HIGH);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, LOW);
+    digitalWrite(RIGHT_R, LOW);
+}
+
+//Turn right slightly
+void turnRightSlight(int speed, int halfSpeed)
+{
+    Serial.println("Moving right");
+    analogWrite(LEFT_SPEED, halfSpeed);
+    analogWrite(RIGHT_SPEED, speed);
+
+    digitalWrite(LEFT_F, HIGH);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, HIGH);
+    digitalWrite(RIGHT_R, LOW);
 }
 
 
@@ -308,29 +316,23 @@ void turnRight(int speed)
 void stop() {
     Serial.println("STOP");
 
-	digitalWrite(LEFT_F, LOW);
-	digitalWrite(LEFT_R, LOW);
-	digitalWrite(RIGHT_F, LOW);
-	digitalWrite(RIGHT_R, LOW);
+    digitalWrite(LEFT_F, LOW);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, LOW);
+    digitalWrite(RIGHT_R, LOW);
 }
 
 //Victory burnout function
 void victorySpins() {
     Serial.println("Get Clapped Nerds!");
     digitalWrite(LEFT_F, LOW);
-	digitalWrite(LEFT_R, HIGH);
-	digitalWrite(RIGHT_F, HIGH);
-	digitalWrite(RIGHT_R, LOW);
+    digitalWrite(LEFT_R, HIGH);
+    digitalWrite(RIGHT_F, HIGH);
+    digitalWrite(RIGHT_R, LOW);
     delay(2000);
     digitalWrite(LEFT_F, HIGH);
-	digitalWrite(LEFT_R, LOW);
-	digitalWrite(RIGHT_F, LOW);
-	digitalWrite(RIGHT_R, HIGH);
+    digitalWrite(LEFT_R, LOW);
+    digitalWrite(RIGHT_F, LOW);
+    digitalWrite(RIGHT_R, HIGH);
     delay(2000);
 }
-
-
-
-
-// Don't forget to ask questions on the DISCORD if you need any help!
-
